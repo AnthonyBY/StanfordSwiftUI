c6826552a23d5e81bfdc5c59853e3e4f85fd75d7
Anthony <anthonymarchenko@gmail.com>
2021-10-13 18:02:38 +0300

    Project init

--- StanfordSwiftUI/ContentView.swift
+++ StanfordSwiftUI/ContentView.swift
@@ -8,15 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
-    var emojis = ["✈️", "⚠️", "☀️", "🐮"]
-
+    var emojis = ["✈️", "⚠️", "☀️", "🐮", "⚠️", "☀️", "🐮", "⚠️", "☀️", "🐮"]
+    @State var emojiCount = 3
+    
     var body: some View {
-        HStack {
-            ForEach(emojis, id: \.self, content: { emoji in
-                CardView(content: emoji)
-            })
+        VStack {
+            HStack {
+                LazyVGrid(columns: [GridItem(), GridItem(), GridItem()], content: {
+                    ForEach(emojis[0...emojiCount], id: \.self, content: { emoji in
+                        CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                    })
+                })
+            }
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .foregroundColor(.yellow)
         }
+        .padding(.horizontal)
         .foregroundColor(.red)
+    }
+    
+    var remove: some View {
+        Button(action: {
+            emojiCount -= 1
+        }, label: {
+            VStack {
+                Image(systemName: "minus.circle")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(Color.yellow)
+                    .padding(.leading)
+                    .frame(width: 50.0, height: 50.0)
+            }
+        })
+    }
+    
+    var add: some View {
+        Button(action: {
+            emojiCount += 1
+        }, label: {
+            VStack {
+                Image(systemName: "plus.circle")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundColor(Color.yellow)
+                    .padding(.leading)
+                    .frame(width: 50.0, height: 50.0)
+            }
+            .frame(width: 100.0, height: 100.0)
+        })
+        
     }
 }
 
